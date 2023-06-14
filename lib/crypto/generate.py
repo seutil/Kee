57b73ec8d6613667f2f6c9bd2b14af397e038fe5
Ilya Vishnikin <seutil@inbox.ru>
2023-06-14 23:20:50 +0300

    change password function singnature

--- lib/crypto/generate.py
+++ lib/crypto/generate.py
@@ -4,12 +4,17 @@ import secrets
 from Crypto.Random import get_random_bytes
 
 
+ALPHABET = mstring.ascii_letters + mstring.digits + mstring.punctuation + mstring.whitespace
+
+
 def random_bytes(nbytes: int) -> bytes:
     return get_random_bytes(nbytes)
 
 def string(nchars: int) -> str:
     return secrets.token_hex(nchars)
 
-def password(nchars: int) -> str:
-    alphabet = mstring.ascii_letters + mstring.digits + mstring.punctuation + mstring.whitespace
+def password(nchars: int, alphabet = ALPHABET) -> str:
+    if not alphabet:
+        raise ValueError("Empty alphabet")
+
     return ''.join(secrets.choice(alphabet) for _ in range(nchars))

fb6e6e592c7f75304658cfe400eaf913ed86be6c
Ilya Vishnikin <seutil@dvseutil>
2023-06-18 21:43:31 +0300

    added MD5

--- lib/crypto/hasher.py
+++ lib/crypto/hasher.py
@@ -1,11 +1,12 @@
 
 from enum import Enum
-from Crypto.Hash import SHA256 as s256, SHA512 as s512
+from Crypto.Hash import SHA256 as s256, SHA512 as s512, MD5 as md5
 
 
 class ID(Enum):
     SHA256 = "SHA256"
     SHA512 = "SHA512"
+    MD5 = "MD5"
 
 
 class HashInterface:
@@ -25,9 +26,24 @@ def from_id(id_: str) -> HashInterface:
             return SHA256
         case ID.SHA512.value:
             return SHA512
+        case ID.MD5.value:
+            return MD5
         case _:
             return None
 
+class MD5(HashInterface):
+
+    @staticmethod
+    def hash(*data: bytes) -> bytes:
+        h = md5.new()
+        for d in data:
+            h.update(d)
+
+        return h.digest()
+
+    @staticmethod
+    def id() -> ID:
+        return ID.MD5
 
 class SHA256(HashInterface):
 
